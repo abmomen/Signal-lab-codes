5cca2595dbea0948674c029120f356e8643bbc93
abmomin <abdulmomen130@gmail.com>
2018-04-27 20:18:14 -0700

    addition sub mul div modified

--- addition sub mul div.m
+++ addition sub mul div.m
@@ -1,31 +1,130 @@
 clear all;
 close all;
 clc;
-x1=[1 2 3 4];
-subplot(4,1,1);
-stem(x1);
-xlabel('no. of samples');
-ylabel('amplitude');
-title('first signal');
-x2=[4 3 2 1];
-subplot(4,1,2);
-stem(x2);
-xlabel('no. of samples');
-ylabel('amplitude');
-title('second signal');
-x3=x1+x2;
-subplot(4,1,3);
-stem(x3);
-xlabel('no. of samples');
-ylabel('amplitude');
-title('added signal');
-x4=x1*x2;
-subplot(4,1,4);
-stem(x4);
-xlabel('no. of samples');
-ylabel('amplitude');
-title('multiplied signal');
-display(x1);
-display(x2);
-display(x3);
-display(x4);
+m=input('How many inputs : ');
+for i=1:1:m
+    y(i)=input('');
+    yy(i)=y(i);
+end
+ n=input('Enter Zero index : ');
+ p=1-n;
+ Fdif=p
+for i=1:1:m
+    x(i)=p;
+    xx(i)=x(i);
+    p=p+1;
+end
+Ldif=p-1
+m1=input('How many inputs : ');
+for i=1:1:m1
+    y1(i)=input('');
+    yy1(i)=y1(i);
+end
+ n1=input('Enter Zero index : ');
+ p1=1-n1; 
+ Fdif1=p1
+for i=1:1:m1
+    x1(i)=p1;
+    xx1(i)=x1(i);
+    p1=p1+1;
+end
+ Ldif1=p1-1
+if Fdif<Fdif1
+    min=Fdif;
+else
+    min=Fdif1;
+end
+check=m;
+check1=m1;
+if Fdif>min
+    difference=Fdif-min;
+    if difference<0
+        difference=difference*(-1);
+    end
+     check=m+difference;
+    for i=m:-1:difference-1
+        y(i+difference)=y(i);
+    end
+    for i=difference:-1:1
+        y(i)=0;
+    end 
+   else
+    difference=Fdif1-min;
+    if difference<0
+        difference=difference*(-1);
+    end
+    check1=m1+difference;
+    for i=m1:-1:difference-1
+        y1(i+difference)=y1(i);
+    end
+    for i=difference:-1:1
+        y1(i)=0;
+    end
+end
+if check<check1
+    for i=check+1:1:check1
+        y(i)=0;
+    end
+else
+    for i=check1+1:1:check
+        y1(i)=0;
+    end
+end
+ch=max(check,check1);
+for i=1:1:ch
+    c(i)=min;
+    min=min+1;
+end
+for i=1:1:ch
+    sum(i)=y(i)+y1(i);
+    sub(i)=y(i)-y1(i);
+    mul(i)=y(i)*y1(i);
+    div(i)=y(i)/y1(i);
+end
+subplot(4,2,1);
+stem(xx,yy);
+xlabel('Times');
+ylabel('Amplitude');
+title('Input First Signal');
+subplot(4,2,2);
+stem(xx1,yy1);
+xlabel('Times');
+ylabel('Amplitude');
+title('Input Second Signal');
+subplot(4,2,3);
+stem(c,y);
+xlabel('Times');
+ylabel('Amplitude');
+title('Modified First Signal');
+subplot(4,2,4);
+stem(c,y1);
+xlabel('Times');
+ylabel('Amplitude');
+title('Modified Second Signal');
+subplot(4,2,5);
+stem(c,sum);
+xlabel('Times');
+ylabel('Amplitude');
+title('Summation Of These signal');
+subplot(4,2,6);
+stem(c,sub);
+xlabel('Times');
+ylabel('Amplitude');
+title('Subtarction Of These signal');
+subplot(4,2,7);
+stem(c,mul);
+xlabel('Times');
+ylabel('Amplitude');
+title('Multiplication Of These signal');
+subplot(4,2,8);
+stem(c,div);
+xlabel('Times');
+ylabel('Amplitude');
+title('Division Of These signal');
+ y
+y1   
+sum
+sub
+mul
+div
+end
